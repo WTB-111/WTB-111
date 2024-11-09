4b49c4abdff4e5657648b0af89cba99c9ad09db3
WTB-111 <15181957072@163.com>
2024-11-09 18:25:55 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,16 @@
 //王庭宝
 #include <stdio.h>
 int main()
-{
+{   int x;
+	int i;
+	int cj;
+	x=0;
+	cj=1;
+	scanf("%d",&x);
+	for(i=0;i<x;i++){
+		cj=cj*(x-i);
+	};
+	printf("%d",cj);
     printf("Hello World");
     return 0;
 }
